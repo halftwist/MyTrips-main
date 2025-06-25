87351ed7f70bfbc1e225dea75136ee44a5581a31
John Kearon <jkearon@hotmail.com>
2025-06-25 10:20:51 -0400

    completed 4. MapKit with SwiftUI - MapManager and Searching

--- MyTrips/Destinations Tab/DestinationLocationsMapView.swift
+++ MyTrips/Destinations Tab/DestinationLocationsMapView.swift
@@ -8,43 +8,124 @@
 // Subscribe on YouTube: https://youTube.com/@StewartLynch
 // Buy me a ko-fi:  https://ko-fi.com/StewartLynch
 
-
 import SwiftUI
-import MapKit 
+import MapKit
 import SwiftData
 
 struct DestinationLocationsMapView: View {
+    @Environment(\.modelContext) private var modelContext
     @State private var cameraPosition: MapCameraPosition = .automatic
     @State private var visibleRegion: MKCoordinateRegion?
-    @Query private var destinations: [Destination]
-    @State private var destination: Destination?
+    @State private var searchText = ""
+    @FocusState private var searchFieldFocus: Bool  // used for dismissing the search
+    @Query(filter: #Predicate<MTPlacemark> {$0.destination == nil}) private var searchPlacemarks: [MTPlacemark]
+    private var listPlacemarks: [MTPlacemark] {
+        searchPlacemarks + destination.placemarks
+    }
+    var destination: Destination
     
     var body: some View {
-        
+        @Bindable var destination = destination
+        VStack {
+            LabeledContent {
+                TextField("Enter destination name", text: $destination.name)
+                    .textFieldStyle(.roundedBorder)
+                    .foregroundStyle(.primary)
+            } label: {
+                Text("Name")
+            }
+            HStack {
+                Text("Adjust the map to set the region for your destination.")
+                    .foregroundStyle(.secondary)
+                Spacer()
+                Button("Set region") {
+                    if let visibleRegion {
+                        destination.latitude = visibleRegion.center.latitude
+                        destination.longitude = visibleRegion.center.longitude
+                        destination.latitudeDelta = visibleRegion.span.latitudeDelta
+                        destination.longitudeDelta = visibleRegion.span.longitudeDelta
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+            }
+        }
+        .padding(.horizontal)
         Map(position: $cameraPosition) {
-            if let destination {
-                ForEach(destination.placemarks) { placemark in
+            ForEach(listPlacemarks) { placemark in
+                if placemark.destination != nil {
                     Marker(coordinate: placemark.coordinate) {
                         Label(placemark.name, systemImage: "star")
                     }
                     .tint(.yellow)
+                } else {
+                    Marker(placemark.name, coordinate: placemark.coordinate)   // generic marker
                 }
             }
         }
+        .safeAreaInset(edge: .bottom) {
+            HStack {
+                TextField("Search...", text: $searchText)
+                    .textFieldStyle(.roundedBorder)
+                    .focused($searchFieldFocus)
+                    .overlay(alignment: .trailing) {
+                        if searchFieldFocus {
+                            Button {
+                                searchText = ""
+                                searchFieldFocus = false
+                            } label: {
+                                Image(systemName: "xmark.circle.fill")
+                            }
+                            .offset(x: -3)
+                        }
+                    }
+                    .onSubmit {
+                        Task {
+                            await MapManager.searchPlaces(
+                                modelContext,
+                                searchText: searchText,
+                                visibleRegion: visibleRegion
+                            )
+                            searchText = ""
+                        }
+                    }
+                if !searchPlacemarks.isEmpty {
+                    Button {
+                        MapManager.removeSearchResults(modelContext)
+                    } label: {
+                        Image(systemName: "mappin.slash.circle.fill")
+                            .imageScale(.large)
+                    }
+                    .foregroundStyle(.white)
+                    .padding(8)
+                    .background(.red)
+                    .clipShape(.circle)
+                }
+            }
+            .padding()
+        }
+        .navigationTitle("Destination")
+        .navigationBarTitleDisplayMode(.inline)
         .onMapCameraChange(frequency: .onEnd){ context in
             visibleRegion = context.region
         }
-
         .onAppear {
-            destination = destinations.first
-            if let region = destination?.region {
+            MapManager.removeSearchResults(modelContext)
+            if let region = destination.region {
                 cameraPosition = .region(region)
             }
+        }
+        .onDisappear {
+            MapManager.removeSearchResults(modelContext)
         }
     }
 }
 
-#Preview {
-    DestinationLocationsMapView()
-        .modelContainer(Destination.preview)
-}
+//#Preview {
+//    let container = Destination.preview
+//    let fetchDescriptor = FetchDescriptor<Destination>()
+//    let destination = try! container.mainContext.fetch(fetchDescriptor)[0]
+//    return NavigationStack {
+//        DestinationLocationsMapView(destination: destination)
+//    }
+//    .modelContainer(Destination.preview)
+//}
